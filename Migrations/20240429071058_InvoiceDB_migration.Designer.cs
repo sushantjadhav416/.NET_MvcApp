c6be3dd077c5945af54a48838911af137488dd4f
sushant jadhav <sushantjadhav416@gmail.com>
2024-04-29 13:18:31 +0530

    Invoice management backend Updated

--- Migrations/20240429071058_InvoiceDB_migration.Designer.cs
+++ Migrations/20240429071058_InvoiceDB_migration.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MvcApp.Models;
 
@@ -10,9 +11,10 @@ using MvcApp.Models;
 namespace MvcApp.Migrations
 {
     [DbContext(typeof(ProductsContext))]
-    partial class ProductsContextModelSnapshot : ModelSnapshot
+    [Migration("20240429071058_InvoiceDB_migration")]
+    partial class InvoiceDB_migration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Models/product.cs
+++ Models/product.cs
@@ -10,5 +10,9 @@ namespace MvcApp.Models
         public int Id { get; set; }
 
         public string Name { get; set; }
+
+        public int Amount { get; set; }
+
+        public string status  { get; set; }
     }
 }
